4144cb2965ca87fbaf8aae33fdad4fe22316c701
Vesanmartin <ve.sanmartin@duocuc.cl>
2026-05-12 14:25:53 -0400

    integrar auth-service con 2FA y roles corregidos

--- auth-service/src/controllers/auth.controller.js
+++ auth-service/src/controllers/auth.controller.js
@@ -1,39 +1,90 @@
+// src/controllers/auth.controller.js
+
 import { registerUser, loginUser } from "../services/auth.service.js";
 
+let codigoTemporal = "";
+
+// REGISTRO
 export const register = async (req, res) => {
+
   try {
-    const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ error: "Faltan datos" });
-    }
+    const { nombre, email, password } = req.body;
 
-    await registerUser(email, password);
+    const result = await registerUser(
+      nombre,
+      email,
+      password
+    );
 
-    res.status(201).json({
-      message: "Usuario registrado correctamente"
-    });
+    return res.status(201).json(result);
 
   } catch (error) {
-    res.status(400).json({
+
+    return res.status(400).json({
       error: error.message
     });
   }
 };
 
+// LOGIN + 2FA
 export const login = async (req, res) => {
+
   try {
+
     const { email, password } = req.body;
 
+    // validar usuario en BD
     const token = await loginUser(email, password);
 
-    res.json({
-      token
+    // generar código 6 dígitos
+    codigoTemporal = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    console.log("Código generado:", codigoTemporal);
+
+    // NO enviar token todavía
+    return res.status(200).json({
+      success: true,
+      twoFactor: true,
+      message: "El código ha sido enviado a su correo"
     });
 
   } catch (error) {
-    res.status(401).json({
+
+    return res.status(401).json({
+      success: false,
       error: error.message
     });
   }
 };
+
+// VERIFICAR CÓDIGO 2FA
+export const verifyCode = async (req, res) => {
+
+  try {
+
+    const { code } = req.body;
+
+    if (code === codigoTemporal) {
+
+      return res.status(200).json({
+        success: true,
+        message: "Autenticación correcta"
+      });
+    }
+
+    return res.status(401).json({
+      success: false,
+      message: "Código incorrecto"
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+};
+
